e05c29a4378422fb008db6aad1a62d449f2898b0
mithunholla <81265159+mithunholla@users.noreply.github.com>
2021-04-25 22:37:15 +0200

    Store_elements_in_array

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int [] Hello = {11,23,38};
+
+        System.out.println(Hello[0]);
+        System.out.println(Hello[1]);
+        System.out.println(Hello[2]);
+    }
+}
